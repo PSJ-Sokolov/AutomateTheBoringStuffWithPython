138f366a69ec888b94cbce71b9c199d8d491f239
Philip SJ Sokolov <PhilipSokolov@protonmail.com>
2017-11-14 09:34:14 +0100

    Added questions and a file practicing control structures.

---
+++ vampire.py
@@ -0,0 +1,11 @@
+name = "Dracula"
+age  =  4000
+
+if   name == "Alice":
+     print("Hi, Alice.")
+elif age  < 12:
+     print("Y're not Alice, Kiddo!")
+elif age  > 2000:
+     print("Unlike is not an undead, O immortal vampire!")
+elif age  > 100:
+     print("You are not Alice, ye grannie.")
